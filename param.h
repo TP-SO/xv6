9c31653545e08f55d5444baaa7234b33f21ff1f6
gabriel-liraa <gabriel.l.lira26@gmail.com>
2024-05-15 22:36:16 -0300

    Agora Sim Acabou Caralho!

--- param.h
+++ param.h
@@ -11,8 +11,8 @@
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
-#define INTERV 1         // preemption tick rate
+#define INTERV 5         // preemption tick rate
 
-#define CP12 500 
+#define CP12 200 
 #define CP23 300
-#define CP34 200
+#define CP34 500
